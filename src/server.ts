dc3eda6ec378a1942580c47eec59e85c2501a395
Alexandre Abreu <abreuxandi2010@gmail.com>
2022-02-04 21:29:48 -0300

    Debuger config

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
-import express from "express";
+import express, { Request } from "express";
 
 const app = express();
+
+app.use(express.json());
+
+app.post("/courses", (request, response) => {
+  const { name } = request.body;
+
+  return response.json({ name });
+});
 
 app.listen(3000, () => console.log("Server is running !!"));
