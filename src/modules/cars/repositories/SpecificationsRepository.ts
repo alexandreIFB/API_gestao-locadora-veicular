a93ddb62778fec7e7e9c040ecb7b2e66e3ee5b79
Alexandre Abreu <abreuxandi2010@gmail.com>
2022-02-11 04:12:52 -0300

    service create specification

---
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -0,0 +1,33 @@
+import { Specification } from "../model/Specification";
+import {
+  ISpecificationsDTO,
+  ISpecificationsRepository,
+} from "./ISpecificationsRepository";
+
+class SpecificationsRepository implements ISpecificationsRepository {
+  private specifications: Specification[];
+
+  constructor() {
+    this.specifications = [];
+  }
+
+  findByName(name: string): Specification {
+    return this.specifications.find(
+      (specification) => specification.name === name
+    );
+  }
+
+  create({ name, description }: ISpecificationsDTO): void {
+    const specification = new Specification();
+
+    Object.assign(specification, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.specifications.push(specification);
+  }
+}
+
+export { SpecificationsRepository };
